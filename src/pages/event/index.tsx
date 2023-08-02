426bcb75e9ea8c4ebd5af2849d3891e9c17ebf93
michaelsinanta <michaelchristlambert@gmail.com>
2023-08-02 20:40:44 +0700

    fix: auth on event

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import type { NextPage } from "next";
 import { EventModule } from "@/components";
-import { requireAuth } from "@/common/requireAuth";
-
-export const getServerSideProps = requireAuth(async (ctx) => {
-  return { props: {} };
-});
 
 const Event: NextPage = () => <EventModule />;
 
